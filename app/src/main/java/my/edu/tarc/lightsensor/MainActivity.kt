bb664b49a54abae2c7d0e6fb582031517d3bb4c2
Ong Yi Jie <ongyj-wp18@student.tarc.edu.my>
2020-11-12 23:04:13 +0800

    2nd commit

--- app/src/main/java/my/edu/tarc/lightsensor/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/lightsensor/MainActivity.kt
@@ -20,15 +20,28 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private var sensorManager: SensorManager? = null
     private var lightSensor: Sensor? = null
     private lateinit var textViewReading: TextView
+    private lateinit var textViewPower: TextView
+    private lateinit var textViewMaxRange: TextView
+    private lateinit var textViewMaxDelay: TextView
+    private lateinit var textViewMinDelay: TextView
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
         textViewReading = findViewById(R.id.textViewReading)
+        textViewPower = findViewById(R.id.textViewPower)
+        textViewMaxRange = findViewById(R.id.textViewMaxRange)
+        textViewMaxDelay = findViewById(R.id.textViewMaxDelay)
+        textViewMinDelay = findViewById(R.id.textViewMinDelay)
 
         if(event!!.sensor.type == Sensor.TYPE_LIGHT){
             textViewReading.text = event.values[0].toString()
+            textViewPower.text = lightSensor!!.power.toString()
+            textViewMaxRange.text = lightSensor!!.maximumRange.toString()
+            textViewMaxDelay.text = lightSensor!!.maxDelay.toString()
+            textViewMinDelay.text = lightSensor!!.minDelay.toString()
+
         }
     }
 
